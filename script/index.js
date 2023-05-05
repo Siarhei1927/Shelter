4469d6aab39f794e83acf81db44f1019dec9a1a7
Siarhei1927 <shaurylau@gmail.com>
2023-05-05 20:15:05 +0200

    docs: add scripts for pages and array of objects pets.json

---
+++ script/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var body = document.querySelector("body");
+    var menu = document.getElementById("burger");
+    menu.addEventListener("click", function () {
+        document.querySelector("header").classList.toggle("open");
+    })
+    body.addEventListener("click", function(event) {
+        if (event.target != menu) {
+            document.querySelector("header").classList.remove("open"); 
+        }
+    })
+
+       
+    document.getElementById("link-text-color").addEventListener("click", function cozyAlert() {
+        alert ("Welcome to Cozy House")
+    })
+    document.getElementById("inAddition-button").addEventListener("click", function cardAlert() {
+        alert ("Thank You for your donation! You credit card number: 8380 2880 8028 8791 7435")
+    })
+});
+
